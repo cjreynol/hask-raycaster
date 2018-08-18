2d47147c14ff55924209630b393de30ea1693ed9
Chad Reynolds <cjreynol13@aol.com>
2018-08-17 23:33:05 -0500

    Add simple testing input command and tidying

--- src/Main.hs
+++ src/Main.hs
@@ -4,11 +4,15 @@ Description :
 Copyright   : (c) Chad Reynolds, 2018
 License     : MIT
 -}
+
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
-module Main where
+module Main (
+    main
+    ) where
 
-import  Control.Monad   (unless)
+import  Control.Monad   (unless, when)
 import  SDL
 
 
@@ -18,16 +22,21 @@ main = do
     w <- createWindow "Testing" defaultWindow
     r <- createRenderer w (-1) defaultRenderer
     rendererDrawColor r $= V4 0 0 255 255
-    let state = PaintState w r
+    let state = RCState w r 0
     appLoop state
+    quit
 
-appLoop :: PaintState -> IO ()
+appLoop :: RCState -> IO ()
 appLoop state = do
     events <- pollEvents
-    let qPressed = any isQPress events
-    clear $ psRenderer state
-    present $ psRenderer state
-    unless qPressed $ appLoop state
+    let isQuit = any isEscPress events
+    let upPressed = any isUpPress events
+    let nextState = if upPressed then increaseCount state else state
+    clear $ rcRenderer state
+    present $ rcRenderer state
+    when upPressed $ putStrLn $ show . rcCount $ nextState
+    unless isQuit $ appLoop nextState
+
 
 isKeyPress :: Keycode -> Event -> Bool
 isKeyPress keycode event = 
@@ -37,11 +46,17 @@ isKeyPress keycode event =
             keysymKeycode (keyboardEventKeysym keyboardEvent) == keycode
         _ -> False
 
-isQPress :: Event -> Bool
-isQPress = isKeyPress KeycodeQ
+isEscPress :: Event -> Bool
+isEscPress = isKeyPress KeycodeEscape
 
-data PaintState = PaintState { 
-      psWindow        :: Window
-    , psRenderer      :: Renderer
+isUpPress :: Event -> Bool
+isUpPress = isKeyPress KeycodeUp
+
+data RCState = RCState { 
+      rcWindow      :: Window
+    , rcRenderer    :: Renderer
+    , rcCount       :: Int
     }
 
+increaseCount :: RCState -> RCState
+increaseCount RCState{..} = RCState rcWindow rcRenderer (rcCount + 1)
