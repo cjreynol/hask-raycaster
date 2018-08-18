6791fbf5529cd339273019d0127201ee23cfb4da
Chad Reynolds <cjreynol13@aol.com>
2018-08-18 11:21:00 -0500

    Add moving image on the screen example

--- src/Main.hs
+++ src/Main.hs
@@ -12,17 +12,22 @@ module Main (
     main
     ) where
 
-import  Control.Monad   (unless, when)
-import  SDL
+import Control.Monad    (unless)
+import Foreign.C.Types  (CInt)
+
+import SDL
 
 
 main :: IO ()
 main = do
     initializeAll
     w <- createWindow "Testing" defaultWindow
-    r <- createRenderer w (-1) defaultRenderer
-    rendererDrawColor r $= V4 0 0 255 255
-    let state = RCState w r 0
+    r <- createRenderer w (-1) $ RendererConfig SoftwareRenderer False
+    s <- getWindowSurface w
+    i <- loadBMP "assets/guy.bmp" 
+    let x =  P (V2 0 0)
+    rendererDrawColor r $= V4 255 255 255 255
+    let state = RCState w r s i x
     appLoop state
     quit
 
@@ -31,12 +36,17 @@ appLoop state = do
     events <- pollEvents
     let isQuit = any isEscPress events
     let upPressed = any isUpPress events
-    let nextState = if upPressed then increaseCount state else state
-    clear $ rcRenderer state
-    present $ rcRenderer state
-    when upPressed $ putStrLn $ show . rcCount $ nextState
+    let nextState = if upPressed then movePos moveSpeed state else state
+
+    draw nextState
     unless isQuit $ appLoop nextState
 
+draw :: RCState -> IO ()
+draw state = do
+            clear $ rcRenderer state
+            _ <- surfaceBlit (rcImage state) Nothing (rcScreen state) (Just $ rcPos state)
+            updateWindowSurface $ rcWindow state
+            present $ rcRenderer state
 
 isKeyPress :: Keycode -> Event -> Bool
 isKeyPress keycode event = 
@@ -55,8 +65,14 @@ isUpPress = isKeyPress KeycodeUp
 data RCState = RCState { 
       rcWindow      :: Window
     , rcRenderer    :: Renderer
-    , rcCount       :: Int
+    , rcScreen      :: Surface
+    , rcImage       :: Surface
+    , rcPos         :: Point V2 CInt
     }
 
-increaseCount :: RCState -> RCState
-increaseCount RCState{..} = RCState rcWindow rcRenderer (rcCount + 1)
+movePos :: V2 CInt -> RCState -> RCState
+movePos delta RCState{..} = RCState rcWindow rcRenderer rcScreen rcImage (rcPos + (P delta))
+
+moveSpeed :: V2 CInt
+moveSpeed = V2 5 5
+
